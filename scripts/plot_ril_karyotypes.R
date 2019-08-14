27619058a5cb3e7d95748ddc7994b0cf6cf25fb6
rdcoletta <della028@umn.edu>
2019-08-14 10:25:00 -0500

    selected 5 random RILs per cross to plot karyotypes

--- scripts/plot_ril_karyotypes.R
+++ scripts/plot_ril_karyotypes.R
@@ -86,63 +86,65 @@ centros <- data.frame(chr = centros$chr,
 
 
 
-#### load marker positions ----
-
-# select a cross that has a RIL with high AA or BB genotype frequency
-RIL.pop.list <- list.dirs(path = "analysis/qc/", full.names = FALSE, recursive = FALSE)
-cross <- RIL.pop.list[1]
-RIL <- "RIL_3"
-
-# load data with marker positions
-markers.filename <- paste0("analysis/qc/", cross, "/recomb-freq_", cross, "_rils.txt")
-markers.infile <- fread(markers.filename, header = TRUE, data.table = FALSE)
-
-# load genotypic data for all RILs in the cross
-geno.data.filename <- paste0("analysis/qc/", cross, "/geno-data_", cross, "_after_filtering.txt")
-geno.data.infile <- fread(geno.data.filename, header = TRUE, data.table = FALSE)
-
-# merge information of RIL of interest with respective marker positions
-geno.data <- cbind(markers.infile[, c("marker", "chr", "pos")], geno.data.infile[, RIL])
-colnames(geno.data)[4] <- "geno"
-
-# select only AA genotype
-geno.data.AA <- subset(geno.data, geno == "AA")
-geno.data.BB <- subset(geno.data, geno == "BB")
-
-# get genotype frequency to add in the plot
-geno.freq.AA <- NROW(geno.data.AA) / (NROW(geno.data.AA) + NROW(geno.data.BB))
-geno.freq.BB <- NROW(geno.data.BB) / (NROW(geno.data.AA) + NROW(geno.data.BB))
-
-
-
 #### plot karyotypes ----
 
-karyo.plot <- ggplot() +
-  geom_segment(data = chrms,
-               aes(x = 0, xend = 0, y = start_pos, yend = end_pos),
-               lineend = "round", color = "Gainsboro", size = 5) +
-  geom_segment(data = centros,
-               aes(x = 0, xend = 0, y = start_pos, yend = end_pos),
-               lineend = "round", color = "DimGray", size = 5) +
-  geom_segment(data = geno.data.AA,
-               aes(x = 0, xend = 0, y = pos, yend = pos + 1e6),  # increased size to be able to see the marker
-               lineend = "butt", color = "darkblue", size = 5, alpha = 0.2) +
-  geom_segment(data = geno.data.BB,
-               aes(x = 0, xend = 0, y = pos, yend = pos + 1e6),  # increased size to be able to see the marker
-               lineend = "butt", color = "firebrick", size = 5, alpha = 0.2) +
-  scale_y_reverse(breaks = seq(0, 3.5e8, 0.50e8), labels = c(1, seq(50, 350, 50))) +
-  scale_x_discrete(position = "top") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        plot.caption = element_text(size = rel(1.1), color = "DimGray")) + 
-  facet_grid(cols = vars(chr), switch = "y") +
-  labs(caption = paste0(cross, " - ", gsub("RIL_", "RIL ", RIL), "\n\n",
-                        "AA freq (blue): ", round(geno.freq.AA, digits = 2), "\n",
-                        "BB freq (red): ", round(geno.freq.BB, digits = 2)),
-       x = "Chromosomes", y = "Genomic positions (Mb)")
+# get names of RIL populations
+RIL.pop.list <- list.dirs(path = "analysis/qc/", full.names = FALSE, recursive = FALSE)
 
-karyo.name <- paste0("analysis/qc/", cross, "/karyotype_", cross, "_", gsub("RIL_", "ril-", RIL),".png")
-ggsave(filename = karyo.name, plot = karyo.plot, device = "png")
+for (RIL.pop in RIL.pop.list) {
+  # load data with marker positions
+  markers.filename <- paste0("analysis/qc/", RIL.pop, "/recomb-freq_", RIL.pop, "_rils.txt")
+  markers.infile <- fread(markers.filename, header = TRUE, data.table = FALSE)
+  
+  # load genotypic data for all RILs in the RIL population
+  geno.data.filename <- paste0("analysis/qc/", RIL.pop, "/geno-data_", RIL.pop, "_after_filtering.txt")
+  geno.data.infile <- fread(geno.data.filename, header = TRUE, data.table = FALSE)
+  
+  # randomly select 5 RILs per population to plot karyotype
+  set.seed(184)
+  selected.RILs <- sample(colnames(geno.data.infile[-1]), size = 5, replace = FALSE)
+  
+  for (RIL in selected.RILs) {
+    # merge information of RIL of interest with respective marker positions
+    geno.data <- cbind(markers.infile[, c("marker", "chr", "pos")], geno.data.infile[, RIL])
+    colnames(geno.data)[4] <- "geno"
+    
+    # select only AA genotype
+    geno.data.AA <- subset(geno.data, geno == "AA")
+    geno.data.BB <- subset(geno.data, geno == "BB")
+    
+    # get genotype frequency to add in the plot
+    geno.freq.AA <- NROW(geno.data.AA) / (NROW(geno.data.AA) + NROW(geno.data.BB))
+    geno.freq.BB <- NROW(geno.data.BB) / (NROW(geno.data.AA) + NROW(geno.data.BB))
+    
+    karyo.plot <- ggplot() +
+      geom_segment(data = chrms,
+                   aes(x = 0, xend = 0, y = start_pos, yend = end_pos),
+                   lineend = "round", color = "Gainsboro", size = 5) +
+      geom_segment(data = centros,
+                   aes(x = 0, xend = 0, y = start_pos, yend = end_pos),
+                   lineend = "round", color = "DimGray", size = 5) +
+      geom_segment(data = geno.data.AA,
+                   aes(x = 0, xend = 0, y = pos, yend = pos + 1e6),  # increased size to be able to see the marker
+                   lineend = "butt", color = "darkblue", size = 5, alpha = 0.2) +
+      geom_segment(data = geno.data.BB,
+                   aes(x = 0, xend = 0, y = pos, yend = pos + 1e6),  # increased size to be able to see the marker
+                   lineend = "butt", color = "firebrick", size = 5, alpha = 0.2) +
+      scale_y_reverse(breaks = seq(0, 3.5e8, 0.50e8), labels = c(1, seq(50, 350, 50))) +
+      scale_x_discrete(position = "top") +
+      theme_minimal() +
+      theme(axis.text.x = element_blank(),
+            axis.ticks.x = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.caption = element_text(size = rel(1.1), color = "DimGray")) + 
+      facet_grid(cols = vars(chr), switch = "y") +
+      labs(caption = paste0(RIL.pop, " - ", gsub("RIL_", "RIL ", RIL), "\n\n",
+                            "AA freq (blue): ", round(geno.freq.AA, digits = 2), "\n",
+                            "BB freq (red): ", round(geno.freq.BB, digits = 2)),
+           x = "Chromosomes", y = "Genomic positions (Mb)")
+    
+    karyo.name <- paste0("analysis/qc/", RIL.pop, "/karyotype_", RIL.pop, "_", gsub("RIL_", "ril-", RIL),".png")
+    ggsave(filename = karyo.name, plot = karyo.plot, device = "png")
+  }
+}
