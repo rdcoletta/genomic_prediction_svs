39ee24c37d5fbb6ed2f8c1f354ec8abdf3e40db0
rdcoletta <della028@umn.edu>
2021-02-04 22:11:54 -0600

    adjusted bash variables to new simulation R script

--- scripts/trait_simulation.sh
+++ scripts/trait_simulation.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
-#PBS -l walltime=2:00:00,nodes=1:ppn=1,mem=100gb
-#PBS -o /home/hirschc1/della028/projects/genomic_prediction/simulation/analysis/trait_sim
-#PBS -e /home/hirschc1/della028/projects/genomic_prediction/simulation/analysis/trait_sim
-#PBS -V
-#PBS -N trait_simulation
-#PBS -M della028@umn.edu
-#PBS -m abe
-#PBS -r n
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=100gb
+#SBATCH -J trait_simulation
+#SBATCH -o /home/hirschc1/della028/projects/genomic_prediction/simulation/analysis/trait_sim/MSI_dump/%x_%j.out
+#SBATCH -e /home/hirschc1/della028/projects/genomic_prediction/simulation/analysis/trait_sim/MSI_dump/%x_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=della028@umn.edu
+#SBATCH --no-requeue
 
 module load R/3.6.0
+
+# check for optional variables -- if not requested, set it blank
+
+# SV options
+[[ -z ${RATIO} ]] && RATIO="" || RATIO="--SNP-SV-ratio=${RATIO}"
+[[ -z ${SVEFFECT} ]] && SVEFFECT="" || SVEFFECT="--SV-effect=${SVEFFECT}"
+# mult env options
+[[ -z ${GENCOR} ]] && GENCOR="" || GENCOR="--gen-cor-matrix=${GENCOR}"
+[[ -z ${RESCOR} ]] && RESCOR="" || RESCOR="--res-cor-matrix=${RESCOR}"
+[[ -z ${GXE} ]] && GXE="" || GXE="--${GXE}"
+[[ -z ${DIFFMEAN} ]] && DIFFMEAN="" || DIFFMEAN="--${DIFFMEAN}"
+# report option
+[[ -z ${QTNVAR} ]] && QTNVAR="" || QTNVAR="--${QTNVAR}"
 
 # go to project folder
 cd ~/projects/genomic_prediction/simulation
 
-# qc hapmap file
+# simulate traits
 Rscript scripts/trait_simulation.R ${HMP} ${SVS} ${FOLDER} \
-                                   --causal-variant=${VAR} \
-                                   --rep=${REPS} \
-                                   --ntraits=${ENVS} \
+                                   --causal-variant=${VAR} ${RATIO} \
+                                   --pops=${POPS} \
+                                   --reps=${REPS} \
+                                   --envs=${ENVS} \
                                    --h2=${H2} \
                                    --model=${MODEL} \
                                    --add-QTN-num=${QTN} \
-                                   --add-effect=${EFFECT} \
+                                   --add-effect-type=${EFFECTTYPE} \
+                                   --marker-effect=${EFFECTSIZE} ${SVEFFECT} \
                                    --architecture=pleiotropic \
-                                    --seed=${SEED}
+                                   --seed=${SEED} \
+                                   ${GENCOR} ${RESCOR} ${GXE} ${DIFFMEAN} ${QTNVAR}
