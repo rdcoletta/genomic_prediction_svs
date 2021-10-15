587faf1435cea60e8690a329c985426514edd6e4
rdcoletta <della028@umn.edu>
2021-10-15 10:40:28 -0500

    transition from pbs to slurm scheduler

--- scripts/hmp2plk.sh
+++ scripts/hmp2plk.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#PBS -l walltime=2:00:00,nodes=1:ppn=1,mem=110gb
-#PBS -o /home/hirschc1/della028/projects/genomic_prediction/simulation/analysis/ld
-#PBS -e /home/hirschc1/della028/projects/genomic_prediction/simulation/analysis/ld
-#PBS -V
-#PBS -N hmp2plk_${CHR}
-#PBS -M della028@umn.edu
-#PBS -m abe
-#PBS -r n
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=110gb
+#SBATCH -J hmp2plk_${CHR}
+#SBATCH -o /home/hirschc1/della028/projects/genomic_prediction/simulation/analysis/ld/%x_%j.out
+#SBATCH -e /home/hirschc1/della028/projects/genomic_prediction/simulation/analysis/ld/%x_%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=della028@umn.edu
+#SBATCH --no-requeue
 
 # go to project folder
 cd ~/projects/genomic_prediction/simulation
