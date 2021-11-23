a8b1d3a5d74fff84bdc5d5d4f930d946f71e80cd
rdcoletta <della028@umn.edu>
2021-11-23 09:42:58 -0600

    removed empty spaces

--- scripts/correlate_ld_with_accuracy.R
+++ scripts/correlate_ld_with_accuracy.R
@@ -19,7 +19,7 @@ optional argument:
   --help                        show this helpful message
   --trait-pops=[LIST]           comma-separated list of pop_ns of QTNs used for simulating traits
   --pred-iters=[LIST]           comma-separated list of predictors iterations used in genomic prediction
-  
+
 note: make sure that to provide the same number of `--trait-pops` and `--pred-iters`
 
 "
@@ -27,25 +27,25 @@ note: make sure that to provide the same number of `--trait-pops` and `--pred-it
 }
 
 getArgValue <- function(arg) {
-  
+
   # get value from command-line arguments
   arg <- unlist(strsplit(arg, "="))
   if (length(arg) == 1) return(TRUE)
   if (length(arg) > 1) return(arg[2])
-  
+
 }
 
 getMarkersHighestLD <- function(ld_summary) {
-  
+
   # create empty df to store results
   highest_ld_results <- data.frame()
-  
+
   # get predictor in highest LD with a causal variant
   for (var in unique(ld_summary$causal_var)) {
-    
+
     # keep only respective causal variant
     ld_summary_causal <- subset(ld_summary, causal_var == var)
-    
+
     # if all r2 are NA (i.e. no predictor in ld)...
     if (sum(is.na(ld_summary_causal$r2)) == NROW(ld_summary_causal)) {
       # ...get closest marker position
@@ -59,16 +59,16 @@ getMarkersHighestLD <- function(ld_summary) {
         pred_highest_ld <- pred_highest_ld[which.min(abs(pred_highest_ld$bp_distance)), ]
       }
     }
-    
+
     # append to results df
     highest_ld_results <- rbind(highest_ld_results, pred_highest_ld)
-    
+
   }
   # change second column name
   colnames(highest_ld_results)[2] <- "predictor_highest_ld"
-  
+
   return(highest_ld_results)
-  
+
 }
 
 
@@ -91,12 +91,12 @@ if ("--help" %in% args) usage() & q(save = "no")
 if (length(args) < 3) stop(usage(), "missing positional argument(s)")
 
 if (length(args) > 3) {
-  
+
   opt_args <- args[-1:-3]
   opt_args_allowed <- c("--trait-pops", "--pred-iters")
   opt_args_requested <- as.character(sapply(opt_args, function(x) unlist(strsplit(x, split = "="))[1]))
   if (any(!opt_args_requested %in% opt_args_allowed)) stop(usage(), "wrong optional argument(s)")
-  
+
   # change default based on the argument provided
   for (argument in opt_args_allowed) {
     if (any(grepl(argument, opt_args_requested))) {
@@ -105,7 +105,7 @@ if (length(args) > 3) {
       assign(arg_name, arg_value)
     }
   }
-  
+
 }
 
 # adjust format from optional arguments
@@ -138,11 +138,11 @@ for (trait_var in c("SNP", "SV")) {
   for (trait_qtn in qtn_opts) {
     for (pred_type in predictor_opts) {
       for (i in 1:length(trait_pops)) {
-        
+
         # get qtn pops and pred iters
         trait_pop <- trait_pops[i]
         pred_n <- pred_iters[i]
-        
+
         # load ld summary file
         ld_summary_file <- paste0(folder_results, "/", trait_qtn, "-QTNs_from_",
                                   trait_var, "/ld_causative-vars_predictors/pop",
@@ -150,36 +150,36 @@ for (trait_var in c("SNP", "SV")) {
                                   "_markers.pred-iter", pred_n, ".causal-pop",
                                   trait_pop, ".txt")
         ld_summary <- fread(ld_summary_file, header = TRUE, data.table = FALSE)
-        
+
         # combine all ld summaries into one data frame
         ld_summary_all_scenarios <- rbind(ld_summary_all_scenarios,
                                           cbind(data.frame(qtn = trait_qtn, var = trait_var,
                                                            ratio = trait_ratio, pop = trait_pop,
                                                            pred_type = pred_type, pred_iter = pred_n),
                                                 ld_summary))
-        
+
         for (trait_h2 in h2_opts) {
-          
+
           # get accuracy for each particular simulated and predicted scenario
-          pred_accuracy_scenario <- pred_accuracy %>% 
+          pred_accuracy_scenario <- pred_accuracy %>%
             filter(h2 == trait_h2, qtn == trait_qtn, var == trait_var, is.na(ratio),
                    is.na(sv_effect), is.na(diff_dist), pop == trait_pop,
-                   predictor == pred_type, pred_iter == pred_n) %>% 
-            pivot_longer(!(h2:mean_upperCI), names_to = "stat", values_to = "vals") %>% 
-            select(!(mean_accuracy_envs:mean_upperCI)) %>% 
+                   predictor == pred_type, pred_iter == pred_n) %>%
+            pivot_longer(!(h2:mean_upperCI), names_to = "stat", values_to = "vals") %>%
+            select(!(mean_accuracy_envs:mean_upperCI)) %>%
             separate(stat, into = c("env", "stat"), sep = "_") %>%
             pivot_wider(names_from = "stat", values_from = "vals") %>%
             as.data.frame()
-          
+
           # load pve per qtn summary file
           pve_qtn_summary_file <- paste0(folder_results, "/", trait_qtn, "-QTNs_from_",
                                          trait_var, "/", trait_h2, "-heritability/pop",
                                          trait_pop, "/summary_var_explained_per_qtn.txt")
           pve_qtn_summary <- fread(pve_qtn_summary_file, header = TRUE, data.table = FALSE)
-          
+
           # get predictors in highest ld to causative variants
           highest_ld_results <- getMarkersHighestLD(ld_summary)
-          
+
           # change column name to facilitate merging
           colnames(pve_qtn_summary)[2] <- "causal_var"
           # merge ld results with qtn effects
@@ -190,7 +190,7 @@ for (trait_var in c("SNP", "SV")) {
           accuracy_ld_pve_scenario <- merge(pred_accuracy_scenario, qtn_pred_ld_pve, by = "env", all = TRUE)
           # append to final df
           results_accuracy_ld_pve <- rbind(results_accuracy_ld_pve, accuracy_ld_pve_scenario)
-          
+
         }
       }
     }
@@ -203,11 +203,11 @@ for (trait_qtn in qtn_opts) {
   for (pred_type in predictor_opts) {
     for (trait_ratio in ratio_opts) {
       for (i in 1:length(trait_pops)) {
-        
+
         # get qtn pops and pred iters
         trait_pop <- trait_pops[i]
         pred_n <- pred_iters[i]
-        
+
         # load ld filename
         ld_summary_file <- paste0(folder_results, "/", trait_qtn, "-QTNs_from_",
                                   trait_var, "/SNP-SV-ratio_", trait_ratio,
@@ -215,29 +215,29 @@ for (trait_qtn in qtn_opts) {
                                   "/ld_summary.", pred_type, "_markers.pred-iter", pred_n,
                                   ".causal-pop", trait_pop, ".txt")
         ld_summary <- fread(ld_summary_file, header = TRUE, data.table = FALSE)
-        
+
         # combine all ld summaries into one data frame
         ld_summary_all_scenarios <- rbind(ld_summary_all_scenarios,
                                           cbind(data.frame(qtn = trait_qtn, var = trait_var,
                                                            ratio = trait_ratio, pop = trait_pop,
                                                            pred_type = pred_type, pred_iter = pred_n),
                                                 ld_summary))
-        
+
         for (trait_h2 in h2_opts) {
           for (trait_sv_effect in sv_effect_opts) {
             for (trait_diff_dist in c(FALSE, TRUE)) {
-              
+
               # get accuracy for each particular simulated and predicted scenario
-              pred_accuracy_scenario <- pred_accuracy %>% 
+              pred_accuracy_scenario <- pred_accuracy %>%
                 filter(h2 == trait_h2, qtn == trait_qtn, var == trait_var, ratio == trait_ratio,
                        sv_effect == trait_sv_effect, diff_dist == trait_diff_dist,
-                       pop == trait_pop, predictor == pred_type, pred_iter == pred_n) %>% 
-                pivot_longer(!(h2:mean_upperCI), names_to = "stat", values_to = "vals") %>% 
-                select(!(mean_accuracy_envs:mean_upperCI)) %>% 
+                       pop == trait_pop, predictor == pred_type, pred_iter == pred_n) %>%
+                pivot_longer(!(h2:mean_upperCI), names_to = "stat", values_to = "vals") %>%
+                select(!(mean_accuracy_envs:mean_upperCI)) %>%
                 separate(stat, into = c("env", "stat"), sep = "_") %>%
                 pivot_wider(names_from = "stat", values_from = "vals") %>%
                 as.data.frame()
-              
+
               # load pve per qtn summary file
               pve_qtn_summary_file <- paste0(folder_results, "/", trait_qtn, "-QTNs_from_",
                                              trait_var, "/SNP-SV-ratio_", trait_ratio,
@@ -245,10 +245,10 @@ for (trait_qtn in qtn_opts) {
                                              trait_h2, "-heritability/pop", trait_pop,
                                              "/summary_var_explained_per_qtn.txt")
               pve_qtn_summary <- fread(pve_qtn_summary_file, header = TRUE, data.table = FALSE)
-              
+
               # get predictors in highest ld to causative variants
               highest_ld_results <- getMarkersHighestLD(ld_summary)
-              
+
               # change column name to facilitate merging
               colnames(pve_qtn_summary)[2] <- "causal_var"
               # merge ld results with qtn effects
@@ -259,7 +259,7 @@ for (trait_qtn in qtn_opts) {
               accuracy_ld_pve_scenario <- merge(pred_accuracy_scenario, qtn_pred_ld_pve, by = "env", all = TRUE)
               # append to final df
               results_accuracy_ld_pve <- rbind(results_accuracy_ld_pve, accuracy_ld_pve_scenario)
-              
+
             }
           }
         }
@@ -269,10 +269,10 @@ for (trait_qtn in qtn_opts) {
 }
 
 # sort columns
-ld_summary_all_scenarios <- ld_summary_all_scenarios %>% 
+ld_summary_all_scenarios <- ld_summary_all_scenarios %>%
   arrange(qtn, var, ratio, pop, pred_type, pred_iter, chr)
 
-results_accuracy_ld_pve <- results_accuracy_ld_pve %>% 
+results_accuracy_ld_pve <- results_accuracy_ld_pve %>%
   arrange(env, h2, qtn, desc(var), ratio, sv_effect, diff_dist, pop, predictor, pred_iter, cv, qtn_number)
 
 # write final results
